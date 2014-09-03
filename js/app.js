7de74a99f06f6142f4d8e716c1eecef957c36a90
Raouf Nabi <raouf.nabi@gmail.com>
2014-09-03 11:56:48 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,42 @@
+
+var taskInput = document.getElementById("new-task"); //new task
+var addButton; // first button
+var incompleteTasksHolder  = document.getElementById("incompleteTasks"); //Incomplete-task
+var completedTasksHolder = document.getById("completedTasks"); //completed-task
+
+//Add a new task
+var addTask = function() {
+
+
+}
+
+
+
+// Edit an existin task
+
+var editTask = function() {
+
+
+}
+
+
+// Delete task
+
+var deleteTask = function() {
+
+
+}
+
+// Complete task
+
+var taskComplete = function() {
+
+
+}
+
+// Incomplete task
+
+var taskIncomplete = function() {
+
+
+}
